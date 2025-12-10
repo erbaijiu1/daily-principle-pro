0d1691d27e849f2e28af0146025e01b8a3ef44ca
yyyhcyang <yyyhcyang@jintengtech.com>
2025-12-10 17:44:52 +0800

    modify prompt

--- server/tmp/download_and_get_txt.py
+++ server/tmp/download_and_get_txt.py
@@ -21,7 +21,7 @@ def process_text(need_deal_txt:  str, model_name: str = "qwen3-235b-a22b-thinkin
     3. [根据需要添加：帮我加上合适的标点符号]
     4. [根据需要添加：如果是繁体中文，帮我转换成简体中文]
     5. 保持原文的口语风格和表达特点，尽量不调整语义，错别字或者词语可以调整。
-    6. 由于是语音转文字，所以，一些词需要整体通过语境判断是不是对的，错误的纠正过来。
+    6. 由于是语音转文字，所以，一些由语音转换来的词需要整体通过语境判断是不是对的，错误的纠正过来。
 
     内容如下：
     %need_deal_txt%
@@ -39,17 +39,20 @@ def process_text(need_deal_txt:  str, model_name: str = "qwen3-235b-a22b-thinkin
 if __name__ == "__main__":
     url_and_file_name_list = [
         {
-            "url": "https://encrypt-k-vod.xet.tech/9764a7a5vodtransgzp1252524126/117df24a5145403705784969404/drm/v.f421220.m3u8?sign=63c5da9ec2da5c533d96e3a71a7077c5&t=691b21b5&us=eJffkoWzFr",
-            "file_name": "铸大钱的故事 （一）2025-11-15",
+            "url": "https://v-vod-k.xiaoeknow.com/a8307e36f7a64aeeae086cc157bb356b/appfvn6my9u7697/video/b_u_d07g5c5st3c629qdmda0/miv5wazs0qn8/drm/main.m3u8?sign=253f621575bf62c44711f09a979a5b5f&t=69382bf5&us=mpQkUjhvxO",
+            "file_name": "日本有没有失去30年（一）20251206",
         },
         {
-            "url": "https://v-vod-k.xiaoeknow.com/a8307e36f7a64aeeae086cc157bb356b/appfvn6my9u7697/video/b_u_d07g5c5st3c629qdmda0/mi16oaf905up/drm/main.m3u8?sign=a7815fcf8f6a84a0443a7729dee35ffa&t=691b2218&us=DZkUNAggum",
-            "file_name": "铸大钱的故事 （二）2025-11-15",
+            "url": "https://material-ali.vod.xiaoe-materials.com/909a41edd31c71f080064531959d0102/c22e8321d239d06faf0a7ee3eaed3fb6-sd-encrypt-stream.m3u8?sign=e32c03baa3a622d2148c51938baf42b8&t=69382c6e&us=meirhAGaVw",
+            "file_name": "日本有没有失去30年（二）20251206",
         },
         {
-            "url": "https://v-vod-k.xiaoeknow.com/a8307e36f7a64aeeae086cc157bb356b/appfvn6my9u7697/video/b_u_d07g5c5st3c629qdmda0/mi16ru7t0hn9/drm/main.m3u8?sign=db9961b8dc453f6dc9a22410d799efb6&t=691b223a&us=RaEQBRaNlj",
-            "file_name": "铸大钱的故事 （三）2025-11-15",
-        },
+            "url": "https://v-vod-k.xiaoeknow.com/a8307e36f7a64aeeae086cc157bb356b/appfvn6my9u7697/video/b_u_d07g5c5st3c629qdmda0/miv61rgr0tav/drm/main.m3u8?sign=8ccadec21485343807fe282624767070&t=69382c8c&us=yYBqQEYSCW",
+            "file_name": "日本有没有失去30年（三）20251206",
+        },{
+            "url": "https://v-vod-k.xiaoeknow.com/a8307e36f7a64aeeae086cc157bb356b/appfvn6my9u7697/video/b_u_d07g5c5st3c629qdmda0/miv651co0cjs/drm/main.m3u8?sign=79aac4a85bf41728051e730c950d7f9e&t=69382ca7&us=mhpYZjPiJl",
+            "file_name": "日本有没有失去30年（四）20251206",
+        }
     ]
     for item in url_and_file_name_list:
         try:
